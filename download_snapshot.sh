d9cfffbf73ce031773108c1d155ba662a54cc8c3
obkwon <okwon78@gmail.com>
2020-02-14 14:24:10 +0900

    refactoring bash shell

--- download_snapshot.sh
+++ download_snapshot.sh
@@ -1,1 +1,16 @@
-git clone https://github.com/javacafe-project/elastic-book-snapshot.git
+
+#!/bin/bash
+
+CUR_DIR="$(cd $(dirname $0) && pwd)"
+SNAPSHOT_DIR="$CUR_DIR/elastic-book-snapshot"
+
+echo snapshot directory: $SNAPSHOT_DIR
+
+
+if [ -d $SNAPSHOT_DIR ]
+then
+    echo snapshots already exit!!!
+else
+    git clone https://github.com/javacafe-project/elastic-book-snapshot.git
+fi
+    
